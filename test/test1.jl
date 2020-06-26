b27170a1c6127c527146eba4b3768d5ac6ee2348
Eric Ford <eford@psu.edu>
2020-06-26 12:15:13 -0400

    try blocks around test so see all results at once

--- test/test1.jl
+++ test/test1.jl
@@ -1,10 +1,13 @@
 using Test
 
+try
 @testset "Testing solution to Q1" begin
    @test (q1_root1 == 1 && q1_root2 == 2) || (q1_root1 == 2 && q1_root2 == 1)
 end; # Q1
+catch
+end
 
-
+try
 @testset "Testing solution to Q2" begin
    @test vertex(1,-3,2).x ≈ 1.5
    @test vertex(1,-3,2).y ≈ -0.25
@@ -13,5 +16,6 @@ end; # Q1
    @test vertex(2,-3,2.125).x ≈ 0.75
    @test vertex(2,-3,2.125).y ≈ 1.0
 end; # Exercise 2
-
+catch
+end
 
